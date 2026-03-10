c2be21bd48df7baadb0e597b864e3ed589efb306
NabilaKhoirunisa <nabilak891@gmail.com>
2026-03-10 15:57:15 +0700

    pertemuan4

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $root= "http://" . $_SERVER['HTTP_HOST'];
-$ROOT.= str_replace(basename($_SERVER['SCRIPT_NAME'])),"",$_SERVER['SCRIPT_NAME']
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 $config['base_url'] = $root;
 
 
